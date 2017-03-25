4c9ec61c1bb5d0855ac89bceab473ed2184089e9
Viktor Gomeniuk <viktor.gomeniuk@gmail.com>
2017-03-25 17:42:46 +0200

    Draw particles on screen with random animation and color change.

--- src/main.cpp
+++ src/main.cpp
@@ -7,22 +7,32 @@
 //============================================================================
 
 #include <iostream>
+#include <memory>
 #include <math.h>
+#include <stdlib.h>
+#include <time.h>
 #include <SDL2/SDL.h>
 #include "Screen.h"
+#include "Swarm.h"
+#include "Particle.h"
+
 
 using namespace particleapp;
 
 
 int main(int argc, char** argv) {
+	// Initialize random generator (used in Particles)
+	std::srand(time(nullptr));
+
 	Screen screen;
 	if (!screen.init()){
 		return 1;
 	}
+	Swarm swarm;
 	while(true){  // Main application loop
 		// Update particles
-		// Draw particles
-		// Check for messages/events
+		screen.clear();
+		swarm.update();
 
 		// Change color over time from 0 to 255 (use different multiplier for each RGB channel)
 		int deltaTime = SDL_GetTicks();
@@ -30,21 +40,35 @@ int main(int argc, char** argv) {
 		int green = (1 + sin(deltaTime * 0.0002)) * 128;
 		int blue = (1 + sin(deltaTime * 0.0003)) * 128;
 
-		for(int y = 0; y < Screen::SCREEN_HEIGHT; ++y){
-			for (int x = 0; x < Screen::SCREEN_WIDTH; ++x){
-				screen.setPixel(x, y, red, green, blue);
-			}
+		// Draw particles
+		const Particle * const pParticles = swarm.getParticles();
+		for (int i = 0; i < Swarm::NPARTICLES; ++i) {
+			Particle particle = pParticles[i];
+			particle.update();
+			int x = (particle.m_x + 1) * Screen::SCREEN_WIDTH / 2;
+			int y = (particle.m_y + 1) * Screen::SCREEN_HEIGHT / 2;
+
+			screen.setPixel(x, y, red, green, blue);
 		}
+
+
+//		for(int y = 0; y < Screen::SCREEN_HEIGHT; ++y){
+//			for (int x = 0; x < Screen::SCREEN_WIDTH; ++x){
+//				screen.setPixel(x, y, red, green, blue);
+//			}
+//		}
 
 		screen.update();
 
 		// Process pending events
 		if (!screen.processEvents()){
+			delete [] pParticles;  // TODO
 			break;
 		}
 
 	}
 	// Clean up
+
 	screen.close();
 	return 0;
 }
